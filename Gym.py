bc8f0f1ca30bc8aba3f991c74169ddc24ebe1557
Steve Vott <54283064+svott03@users.noreply.github.com>
2022-01-12 21:16:19 -0800

    fixed duo authentication

--- Gym.py
+++ Gym.py
@@ -18,12 +18,25 @@ class Google:
   self.driver.find_element_by_xpath('//*[@id="username"]').send_keys(username)
   self.driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
   self.driver.find_element_by_xpath('//*[@id="loginform"]/div/button').click()
+  sleep(5)
+  
+  self.driver.switch_to.frame(self.driver.find_element_by_id("duo_iframe"))
+  self.driver.find_element_by_xpath('//*[@id="passcode"]').click()
+  sleep(4)
+  self.driver.find_element_by_xpath('//*[@id="auth_methods"]/fieldset/div[3]/div/input').send_keys(duo)
+  self.driver.find_element_by_xpath('//*[@id="passcode"]').click()
   sleep(3)
   
-  self.driver.find_element_by_xpath('//*[@id="page-wrapper"]/div/div[1]/div/div[3]/p/a').click()
   sleep(3)
+  count = 0
+  self.driver.get("https://myrecsports.usc.edu/booking/cd93ade2-af9d-4e5f-84e0-06e10711b5ce")
   while True:
+    #self.driver.switch_to.frame(self.driver.find_element_by_xpath('//*[@id="divBookingSlots"]/div[2]/div[1]'))
     html_text = self.driver.page_source
+    #html_text = "https://myrecsports.usc.edu/booking/cd93ade2-af9d-4e5f-84e0-06e10711b5ce"
+    #html_text = url.page_source
+    print(f"TEST{count}")
+    count+=1
     soup = BeautifulSoup(html_text, 'lxml')
     #extract div
     mydivs = soup.find_all("div", class_="booking-slot-item")
@@ -32,15 +45,15 @@ class Google:
         #div str text
         temp = div.get_text()
         #if your time is available
-        if "1:15 - 3:15 PM" in temp:
+        if "8:30 - 10:30 PM" in temp:
             #extracts text
             textLine = div.find('span')
             result = textLine.get_text().strip()
             #checks spots available
             if not "No spots available" in result:
                 #sends phone msg
-                account_sid = 'sid'
-                auth_token = 'token'
+                account_sid = 'account_sid'
+                auth_token = 'auth_token'
                 client = Client(account_sid, auth_token)
                 message = client.messages \
                     .create(
@@ -62,5 +75,6 @@ user=open('New Text Document (3).txt',"r",encoding="utf-8")
 username=str(user.read())
 passw=open('New Text Document (2).txt',"r",encoding="utf-8")   
 password=str(passw.read())
+duo = duo_password
 #instantiates object
 page = Google(username,password)
